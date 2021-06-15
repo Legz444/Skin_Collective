9568ab4b36bdd35428873f0fa1e904ded6b958cd
Allegra✨ <amhilburger@gmail.com>
2021-06-15 12:05:01 -0600

    added product section with Figures from bootstrap

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -17,6 +17,66 @@ const Products = () => {
                 <h6>$$$</h6>
             </Figure.Caption>
         </Figure>
+        <Figure>
+            <Figure.Image
+                width={150}
+                height={150}
+                alt="250x250"
+                src="https://res.cloudinary.com/legz444/image/upload/v1623779760/circle_evjqme.png"
+            />
+            <Figure.Caption>
+                <h5>A little about this product</h5>
+                <h6>$$$</h6>
+            </Figure.Caption>
+        </Figure>
+        <Figure>
+            <Figure.Image
+                width={150}
+                height={150}
+                alt="250x250"
+                src="https://res.cloudinary.com/legz444/image/upload/v1623779760/circle_evjqme.png"
+            />
+            <Figure.Caption>
+                <h5>A little about this product</h5>
+                <h6>$$$</h6>
+            </Figure.Caption>
+        </Figure>
+        <Figure>
+            <Figure.Image
+                width={150}
+                height={150}
+                alt="250x250"
+                src="https://res.cloudinary.com/legz444/image/upload/v1623779760/circle_evjqme.png"
+            />
+            <Figure.Caption>
+                <h5>A little about this product</h5>
+                <h6>$$$</h6>
+            </Figure.Caption>
+        </Figure>
+        <Figure>
+            <Figure.Image
+                width={150}
+                height={150}
+                alt="250x250"
+                src="https://res.cloudinary.com/legz444/image/upload/v1623779760/circle_evjqme.png"
+            />
+            <Figure.Caption>
+                <h5>A little about this product</h5>
+                <h6>$$$</h6>
+            </Figure.Caption>
+        </Figure>
+        <Figure>
+            <Figure.Image
+                width={150}
+                height={150}
+                alt="250x250"
+                src="https://res.cloudinary.com/legz444/image/upload/v1623779760/circle_evjqme.png"
+            />
+            <Figure.Caption>
+                <h5>A little about this product</h5>
+                <h6>$$$</h6>
+            </Figure.Caption>
+        </Figure>
       </div>
     )
 }
